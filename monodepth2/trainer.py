3f8d8d27cf52622aeb2606e84ceac848362e4d48
dylan luyben <bobborsboom@hotmail.com>
2021-03-13 19:54:12 +0100

    edge loss now in batches

--- monodepth2/trainer.py
+++ monodepth2/trainer.py
@@ -393,12 +393,14 @@ class Trainer:
 
         self.set_train()
 
-    def prepare_edge_plot(self, disp, inputs):
+    def prepare_edge_plot(self, disp, inputs, attention_mask):
         """
         Once in a while make a plot of the edge loss. Therefor prepare the min and max values for correct depth mask plot
         retreive the original kitti image
         and set correct dimensions for the depth image
         """
+
+        attention_mask = attention_mask.view(self.opt.height * self.opt.batch_size, self.opt.width)
 
         path = self.opt.log_dir + self.opt.model_name + "/" + "edge_loss_img/"
 
@@ -406,19 +408,29 @@ class Trainer:
         if not os.path.exists(path):
             os.makedirs(path)
 
+
+        disp = disp.view(self.opt.height * self.opt.batch_size, self.opt.width)
+
         disp_min = np.uint8(disp.min().cpu().detach().numpy() * 255)
         disp_max = np.uint8(disp.max().cpu().detach().numpy() * 255)
 
         original_img = inputs["color_aug", 0, 0]
+
+        original_img = original_img.view(self.opt.height * self.opt.batch_size, self.opt.width, 3)
+        # depth_mask = depth_mask.view(self.opt.height * self.opt.batch_size, self.opt.width, 3)
+
         original_img = np.array(original_img.cpu().detach().numpy())
-        original_img = np.swapaxes(original_img, 1, 2)
-        original_img = np.swapaxes(original_img, 2, 3)
+
+
+        # original_img = np.swapaxes(original_img, 1, 2)
+        # original_img = np.swapaxes(original_img, 0, 1)
+
 
         # filter out dimension for correct plotting
         disp = disp.squeeze(1)
         disp = np.array(disp.cpu().detach().numpy())
 
-        return disp_min, disp_max, original_img, disp, path
+        return disp_min, disp_max, original_img, disp, path, attention_mask
 
     def additional_not_overlapping_check(self, attention_mask):
         # additional check if the not overlapping masks are not overlapping
@@ -471,7 +483,26 @@ class Trainer:
             disp, [self.opt.height, self.opt.width], mode="bilinear", align_corners=False)
 
         # create only the depth pixels that lie inside the attention mask
-        depth_mask = attention_mask * disp
+
+        # disp [2, 1, 192, 640]
+
+        # depth mask [2, 1, 192,640
+
+        # sum all the attention mask together in 1 attention tensor for faster computation. This is possible because there are not overlalping maps # attention_mask [2, 1, 192, 640]
+        attention_mask = attention_mask.sum(1).unsqueeze(1)
+
+        # breakpoint()
+        assert attention_mask.max() == 1, "sum of attention masks is greater than one .. probably overlapping masks"
+
+
+        depth_mask = (attention_mask * disp).squeeze(0).squeeze(0)
+
+        # breakpoint()
+        if self.opt.batch_size > 1:
+
+            # 1, 1 192 * batch_size , 640. so paste the images under eachother such that you only have to do edge detection once
+            depth_mask = depth_mask.view(self.opt.height * self.opt.batch_size, self.opt.width)
+
         depth_mask = np.array(depth_mask.cpu().detach().numpy())
 
         # * 255 for canny edge working
@@ -480,63 +511,61 @@ class Trainer:
         # cast to int for correct canny input
         depth_mask = np.uint8(depth_mask)
 
-        # prepare images for plotting
-        if batch_idx % self.opt.save_plot_every == 0 and scale == 0:
-            disp_min, disp_max, original_img, disp, path = self.prepare_edge_plot(disp, inputs)
-
-        for batch in range(self.opt.batch_size):
-
-            # loop over the attention masks per batch
-            for attention in range(depth_mask.shape[1]):
-
-                # sometimes there are no attention masks, then skip because you won't find edges
-                if attention_mask[batch][attention].sum() == 0:
-                    continue
-
-                edges_disp = cv2.Canny(depth_mask[batch][attention], self.opt.edge_detection_threshold * 255, self.opt.edge_detection_threshold * 255, apertureSize=3, L2gradient=False)
-                kernel = np.ones((5, 5), np.uint8)
-                erosion = cv2.erode(depth_mask[batch][attention], kernel, iterations=3)
-                result = cv2.bitwise_and(edges_disp, edges_disp, mask=erosion)
-
-                # if you found an edge and save the image. only once in the self.opt.save_edge_img steps because of computational speed
-                if result.sum() > 0 and batch_idx % self.opt.save_plot_every  == 0 and scale ==0:
-
-                    fig, ax = plt.subplots(7, 1, figsize=(12, 12))
-
-                    ax[0].imshow(original_img[batch])
-                    ax[0].title.set_text('Original image')
-                    ax[0].axis('off')
-
-                    ax[1].imshow(disp[batch])
-                    ax[1].title.set_text('disp')
-                    ax[1].axis('off')
-
-                    ax[2].imshow(original_attention.cpu()[batch][index_nrs_not_overlapping[batch][attention]], cmap='cividis')
-                    ax[2].title.set_text('Original attention mask')
-                    ax[2].axis('off')
-
-                    ax[3].imshow(attention_mask.cpu()[batch][attention], cmap='cividis')
-                    ax[3].title.set_text('Casted attention mask')
-                    ax[3].axis('off')
-
-                    ax[4].imshow(depth_mask[batch][attention], vmin =disp_min, vmax=disp_max)
-                    ax[4].title.set_text('depth mask')
-                    ax[4].axis('off')
-
-                    ax[5].imshow(edges_disp)
-                    ax[5].title.set_text('Edges disp before erosion')
-                    ax[5].axis('off')
-
-                    ax[6].imshow(result)
-                    ax[6].title.set_text('Edges disp after erosion')
-                    ax[6].axis('off')
-
-                    fig.savefig('{}/epoch_{}_batchIDX_{}_attenion_{}_result_{}_threshold_{}.png'.format(path, self.epoch, batch_idx, attention, result.sum()/255, self.opt.edge_detection_threshold))
-                    plt.close()
+        # # prepare images for plotting
+        # if batch_idx % self.opt.save_plot_every == 0 and scale == 0:
+        disp_min, disp_max, original_img, disp, path, attention_mask = self.prepare_edge_plot(disp, inputs, attention_mask)
 
 
-                if result.sum() > 0:
-                    edge_loss.append(result.sum())
+        edges_disp = cv2.Canny(depth_mask, self.opt.edge_detection_threshold * 255, self.opt.edge_detection_threshold * 255, apertureSize=3, L2gradient=False)
+        kernel = np.ones((5, 5), np.uint8)
+        erosion = cv2.erode(depth_mask, kernel, iterations=3)
+        result = cv2.bitwise_and(edges_disp, edges_disp, mask=erosion)
+
+        # if you found an edge and save the image. only once in the self.opt.save_edge_img steps because of computational speed
+        # if result.sum() > 0 and batch_idx % self.opt.save_plot_every  == 0 and scale ==0:
+
+        # fig, ax = plt.subplots(7, 1, figsize=(12, 12))
+        #
+        # # breakpoint()
+        # # 2, 192, 640, 3
+        # ax[0].imshow(original_img)
+        # ax[0].title.set_text('Original image')
+        # ax[0].axis('off')
+        # #
+        # # # 2, 192, 640
+        # ax[1].imshow(disp)
+        # ax[1].title.set_text('disp')
+        # ax[1].axis('off')
+        #
+        # # ax[2].imshow(original_attention.cpu()[batch][index_nrs_not_overlapping[batch][attention]], cmap='cividis')
+        # # ax[2].title.set_text('Original attention mask')
+        # # ax[2].axis('off')
+        #
+        # # #2, 1, 192, 640
+        # ax[3].imshow(attention_mask.cpu(), cmap='cividis')
+        # ax[3].title.set_text('Casted attention mask')
+        # ax[3].axis('off')
+        #
+        # ax[4].imshow(depth_mask, vmin =disp_min, vmax=disp_max)
+        # ax[4].title.set_text('depth mask')
+        # ax[4].axis('off')
+        #
+        # ax[5].imshow(edges_disp)
+        # ax[5].title.set_text('Edges disp before erosion')
+        # ax[5].axis('off')
+        #
+        # ax[6].imshow(result)
+        # ax[6].title.set_text('Edges disp after erosion')
+        # ax[6].axis('off')
+        #
+        # # plt.show()
+        #
+        # fig.savefig('{}/epoch_{}_batchIDX_{}_result_{}_threshold_{}.png'.format(path, self.epoch, batch_idx, result.sum()/255, self.opt.edge_detection_threshold))
+        # plt.close()
+
+
+        if result.sum() > 0:
+            edge_loss.append(result.sum())
 
         edge_loss = torch.FloatTensor(edge_loss).to(self.device)
 
@@ -833,21 +862,27 @@ class Trainer:
                 # this function selects frame ID -1 and +1. 0 is the target image. So it creates two prediction images
 
                 pred = outputs[("color", frame_id, scale)]
+
+
                 # print("LOSS REPRONECTION")
+                start = time.time()
                 reprojection_losses.append(self.compute_reprojection_loss(pred, target, inputs, attention_weight))
+                duration = time.time() - start
+                print("DURATION reprojection", duration)
 
             reprojection_losses = torch.cat(reprojection_losses, 1)
 
             if self.opt.edge_loss:
 
-                # start = time.time()
+                start = time.time()
                 edge_loss = self.edge_detection_loss(scale, outputs, inputs, not_overlapping_attention_masks, batch_idx, index_numbers_not_overlapping, original_masks)
                 # print("edge loss", self.opt.edge_weight * edge_loss / (2 ** scale))
 
-                print("EDGE LOSS", self.opt.edge_weight * edge_loss / (2 ** scale))
+                # print("EDGE LOSS", self.opt.edge_weight * edge_loss / (2 ** scale))
                 loss += self.opt.edge_weight * edge_loss / (2 ** scale)
 
-                # duration = time.time() - start
+                duration = time.time() - start
+                print("DURATION edge", duration)
 
 
             # TRUE
@@ -893,7 +928,7 @@ class Trainer:
             if not self.opt.disable_automasking:
                 # add random numbers to break ties
                 identity_reprojection_loss += torch.randn(
-                    identity_reprojection_loss.shape).to('cuda' if torch.cuda.is_available() else 'cpu') * 0.00001
+                    identity_reprojection_loss.shape).to('cpu' if self.opt.no_cuda else 'cuda') * 0.00001
 
 
                 combined = torch.cat((identity_reprojection_loss, reprojection_loss), dim=1)
