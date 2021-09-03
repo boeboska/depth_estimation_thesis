b87711a8227301b5daaa269234c6aef0d082ab3f
dylan luyben <bobborsboom@hotmail.com>
2021-09-03 09:35:46 +0200

    remove diplicate

--- monodepth_self_attention/datasets/__init__.py
+++
@@ -1,2 +0,0 @@
-from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
-from .cityscapes_dataset import CityscapesData
--- monodepth_self_attention/networks/__init__.py
+++
@@ -1,6 +0,0 @@
-from .resnet_encoder import ResnetEncoder
-from .monodepth2_decoder import DepthDecoder
-from .pose_decoder import PoseDecoder
-from .pose_cnn import PoseCNN
-from .encoder_selfattn import get_resnet101_asp_oc_dsn
-from .decoder import MSDepthDecoder
